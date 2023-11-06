854649e974116d78df1543c8c413c4eca792d560
ALLUK <lam83@bk.ru>
2023-11-06 22:24:08 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppNestedLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, type a number of a task from 1 to 14: ");
+            string s = Console.ReadLine();
+            bool result = int.TryParse(s, out int number);
+
+            try
+            {
+                if (number == 1)
+                {
+                    OneAppTask();
+                }
+                else 
+                {
+                    Console.WriteLine("The number of the task is not corrected. Let`s one more time");
+                }
+                Console.WriteLine($"The process is: {result}");
+                Console.WriteLine( );
+            }
+
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"Processing failed: {ex.Message}");
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Исходное данное - натуральное число q, выражающее площадь. Написать программу для нахождения всех таких прямоугольников, площадь 
+        /// которых равна q и стороны выражены натуральными числами.
+        /// </summary>
+        static void OneAppTask()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
